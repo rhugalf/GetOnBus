0e230ab2fe670c539205749ec7cc5c3670945526
GLB-MXM0004 <glb-mxm0004@MacBook-Pro-de-GLB-MXM0004.local>
2014-10-14 19:29:25 -0500

    Initial Commit

---
+++ GetOnBus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetOnBus
+//
+//  Created by GLB-MXM0004 on 14/10/14.
+//  Copyright (c) 2014 Hugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
